13f6c88dbd9817cca6ffc5e007eea83a3fac1e9f
ks1venberg <venberg.dev@gmail.com>
2018-05-26 18:00:34 +0300

    table structure added , db:migrate

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+end
 
 get '/' do
 	erb :index			
---
+++ db/migrate/20180526142033_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+  	  create_table :products do |t|
+  	  	t.string :title
+  	  	t.text :description
+  	  	t.decimal :price
+  	  	t.decimal :size
+  	  	t.boolean :is_spicy
+  	  	t.boolean :is_veg
+  	  	t.boolean :is_bestoffer
+  	  	t.string :pathtoimage
+
+  		  t.timestamps
+  		end
+  end
+end
