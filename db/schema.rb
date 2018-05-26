992d6a480621c0657ae2ebbf541c7c3732fc103f
ks1venberg <venberg.dev@gmail.com>
2018-05-26 18:59:55 +0300

    ceed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_142033) do
+ActiveRecord::Schema.define(version: 2018_05_26_151456) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
