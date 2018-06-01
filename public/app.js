230795d048f0d0232ec96fcb4629af32473d706b
Konstantin <venberg.dev@gmail.com>
2018-06-01 12:15:05 +0300

    add counter for button step2

--- public/app.js
+++ public/app.js
@@ -19,9 +19,29 @@ function add_to_cart(id)
 		x = x*1 + 1;
 		window.localStorage.setItem(key, x);
 
-	var z = 0
-		Object.keys(localStorage).forEach(function(key){
-   		console.log(localStorage.getItem(key));
-			z = z*1 + x;
-			alert ("You added " + z);})
+		alert ("You added " + x);
+
+
 }
+
+// 	var z = 0
+// 		Object.keys(localStorage).forEach(function(key){
+//    		console.log(localStorage.getItem(key));
+// 			z = z*1 + x;
+// 			alert ("You added " + z);})
+// }
+
+// function add_to_cart(id)
+// {
+// 	var key = 'product_' + id;
+	
+// 	var x = window.localStorage.getItem(key);
+// 		x = x*1 + 1;
+// 		window.localStorage.setItem(key, x);
+
+// 	var z = 0
+// 		Object.keys(localStorage).forEach(function(key){
+//    		console.log(localStorage.getItem(key));
+// 			z = z*1 + x;
+// 			alert ("You added " + z);})
+// }
