06eadfbb1954ef275c19553e3084962f40225fb3
ks1venberg <venberg.dev@gmail.com>
2018-06-08 07:05:39 +0300

    function_parse orders_line

--- dop_app.rb
+++ dop_app.rb
@@ -1,15 +1,22 @@
-orders = "product_1=2, product_2=3, product_3=4"
+ord_line = "product_1=2, product_2=3, product_3=4"
 
-s1 = orders.split(/,/)
+def parse_orders_line ord_line
+	s1 = ord_line.split(/,/)
 
-puts s1.inspect
+	arr = []
 
-s1.each do |x|
-	s2 = x.split(/=/)
-	s3 = s2[0].split(/_/)
+	s1.each do |x|
+		s2 = x.split(/=/)
+		s3 = s2[0].split(/_/)
 
-	key = s3[1]
-	value = s2[1]
+		id = s3[1]
+		cnt = s2[1]
 
-	puts "product id: #{key}, number of items: #{value}"
+		arr2=[id, cnt]
+		arr.push arr2
+	end
+	
+	return arr
 end
+
+puts parse_orders_line(ord_line).inspect
